255d410d99eed20de2561ae4b8d3bb14f7dd596a
TumanGolim <130469755+TumanGolim@users.noreply.github.com>
2024-02-08 18:34:35 +0200

    start

---
+++ src/contacts.js
@@ -0,0 +1,54 @@
+const fs = require("fs/promises");
+const path = require("path");
+
+const contactsPath = path.join(__dirname, "contacts.json");
+
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    return JSON.parse(data);
+  } catch (error) {
+    return [];
+  }
+}
+
+async function getContactById(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const contacts = JSON.parse(data);
+    return contacts.find((contact) => contact.id === contactId) || null;
+  } catch (error) {
+    return null;
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const contacts = JSON.parse(data);
+    const updatedContacts = contacts.filter(
+      (contact) => contact.id !== contactId
+    );
+    await fs.writeFile(contactsPath, JSON.stringify(updatedContacts, null, 2));
+    return updatedContacts.length !== contacts.length
+      ? { id: contactId }
+      : null;
+  } catch (error) {
+    return null;
+  }
+}
+
+async function addContact(name, email, phone) {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const contacts = JSON.parse(data);
+    const newContact = { id: Date.now(), name, email, phone };
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return newContact;
+  } catch (error) {
+    return null;
+  }
+}
+
+module.exports = { listContacts, getContactById, removeContact, addContact };
